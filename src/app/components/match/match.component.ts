61d5558b65af8c8e3b7522a77d9019a6ac3dc1f1
Timofej Jermolaev <teemozy9@gmail.com>
2017-06-24 19:02:17 -0400

    work on profiles

--- src/app/components/match/match.component.ts
+++ src/app/components/match/match.component.ts
@@ -9,21 +9,13 @@ import { BackendService } from '../../services/backend.service';
 export class MatchComponent implements OnInit {
   matches: any[];
 
-  constructor(private backendService: BackendService) { 
-    this.matches =[];
-    this.matches.push({name:"Timofej Jermolaev", description: "IM A BEAST", imgUrl:"https://avatars2.githubusercontent.com/u/11130291?v=3&s=460"});
-    this.matches.push({name:"Chad", description: "I'm an evil beast", imgUrl:"https://images.pexels.com/photos/159655/quarterback-american-football-football-player-passer-159655.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"});
-    this.matches.push({name:"John Doe", description: "I'm an evil beast", imgUrl:"https://images.pexels.com/photos/431962/pexels-photo-431962.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"});
-    this.matches.push({name:"Tim's evil clone", description: "I'm an evil beast", imgUrl:"https://images.pexels.com/photos/206590/pexels-photo-206590.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"});
-    this.matches.push({name:"My Runecape GF", description: "I'm an evil beast", imgUrl:"https://images.pexels.com/photos/323503/pexels-photo-323503.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"});
-    this.matches.push({name:"TESTOR TESTEEER", description: "I'm an evil beast", imgUrl:"https://images.pexels.com/photos/323503/pexels-photo-323503.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"});
-    this.matches.push({name:"James TESTER", description: "I'm an evil beast", imgUrl:"https://images.pexels.com/photos/199102/pexels-photo-199102.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"});
-  }
+  constructor(private backendService: BackendService) {}
 
   ngOnInit() {
     this.backendService.getMatches().subscribe( data => {
+      this.matches = data;
       data.forEach(element => {
-        console.log(element);
+        
       });
     }, err => console.log(err));
   }
