17304ac5c6eb8ee2257d8dfc588f98dd7c3067c8
Timofej Jermolaev <teemozy9@gmail.com>
2017-10-20 02:46:46 -0400

    refactoring image adjust

--- src/app/components/image-adjust/image-adjust.component.ts
+++ src/app/components/image-adjust/image-adjust.component.ts
@@ -11,43 +11,40 @@ export class ImageAdjustComponent implements OnInit  {
   imageData: any;
   @ViewChild('imageCropper') imageCropper;
 
-  
-  
+
   constructor(private backendService: BackendService,
-              private elementRef: ElementRef) { 
+              private elementRef: ElementRef) {
     this.imageData = {};
   }
 
   onFileChanged(fileInput){
     if (fileInput.target.files && fileInput.target.files[0]) {
-        var reader = new FileReader();
+        const reader = new FileReader();
         reader.onload = (e : any) => {
             this.imageData = e.target.result;
-        }
+        };
         reader.readAsDataURL(fileInput.target.files[0]);
     }
   }
 
-  onSubmitImage(){
+  onSubmitImage() {
 
 
-     //locate the file element meant for the file upload.
-    let inputElement: HTMLInputElement = this.elementRef.nativeElement.querySelector('#photo');
-    let fileCount: number = inputElement.files.length;
-    let formData = new FormData();
+    // locate the file element meant for the file upload.
+    const inputElement: HTMLInputElement = this.elementRef.nativeElement.querySelector('#photo');
+    const fileCount: number = inputElement.files.length;
+    const formData = new FormData();
 
-    console.log(inputElement.files.item(0));
-   
-    this.imageCropper.getCropBlob((blob) =>{
-      let image = blob; 
-      if (fileCount > 0) { 
+
+    this.imageCropper.getCropBlob((blob) => {
+      const image = blob;
+      if (fileCount > 0) {
         formData.append('userPhoto', image);
         this.backendService.updateProfilePhoto(formData).subscribe(data =>{
           alert(data.msg);
-        });   
+        });
       }
     });
-    
 
   }
 
